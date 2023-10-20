bd36cd4d45d2c92ec7ea9c5edd9d26cf456ce0e9
eightsuzuki <suzukieight8@gmail.com>
2023-10-20 15:15:01 +0900

    Add static properties to Model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -27,7 +27,6 @@ app.use(errorHandler);
 const start = async () => {
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
