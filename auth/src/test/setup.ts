267982eaaaec5b3927e2ca19d2e36520ae5eb6e2
eightsuzuki <suzukieight8@gmail.com>
2023-10-23 00:50:04 +0900

    Create the Skelton of App

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -9,7 +9,7 @@ declare global {
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfasdf';
+  process.env.JWT_KEY = 'asdf';
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
   const mongo = await MongoMemoryServer.create();
