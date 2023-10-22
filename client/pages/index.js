143ea96502e6a423dbf2a2976935d76cd8f39474
eightsuzuki <suzukieight8@gmail.com>
2023-10-23 02:15:50 +0900

    Create the Code to Judge, GetInitialProps called

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,21 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
 
   return <h1>Landing Page</h1>;
 };
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
-
-//   return response.data;
-// };
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+  }
+  return {};
+};
 
 export default LandingPage;
