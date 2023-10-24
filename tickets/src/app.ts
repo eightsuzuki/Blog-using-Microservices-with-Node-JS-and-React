fd07e5c4b769c0309e53d50b3c31d59baec07c5e
eightsuzuki <suzukieight8@gmail.com>
2023-10-24 15:13:44 +0900

    Better Error Logging

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError, currentUser } from '@rallycoding/common';
+import { errorHandler, NotFoundError, currentUser } from '@suzuki8-tickets/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,6 +18,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
