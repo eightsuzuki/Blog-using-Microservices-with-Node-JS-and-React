45a239395a917936301cd0c1ff1bad7a9c66f38b
eightsuzuki <suzukieight8@gmail.com>
2023-11-11 13:56:54 +0900

    Convenience Document Methods

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@rallycoding/common';
 import { TicketDoc } from './ticket';
+export { OrderStatus };
 
 interface OrderAttrs {
   userId: string;
