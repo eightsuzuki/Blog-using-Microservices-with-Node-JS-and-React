340ce6399863379a674ea36a8368613aaa44b3f9
eightsuzuki <suzukieight8@gmail.com>
2023-11-12 00:01:34 +0900

    Setup fpr Testing Reservation and Publish

--- tickets/src/events/publishers/ticket-updated-publisher.ts
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -1,4 +1,4 @@
-import { Publisher, Subjects, TicketUpdatedEvent } from '@suzuki8-tickets/common';
+import { Publisher, Subjects, TicketUpdatedEvent } from '@rallycoding/common';
 
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
--- tickets/src/routes/show.ts
+++ tickets/src/routes/show.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { NotFoundError } from '@suzuki8-tickets/common';
+import { NotFoundError } from '@rallycoding/common';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
